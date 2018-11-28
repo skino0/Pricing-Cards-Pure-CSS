09f3b83babac7ee159b87814132b9628561e9802
skino0 <skinogigo@gmail.com>
2018-11-28 19:15:55 +0200

    Added Pricing cards Project

---
+++ js/hover.js
@@ -0,0 +1,93 @@
+    
+    /*Card 1*/
+ 
+    $("#card1").hover(
+    function () {
+    $("#cardhalfbg1").addClass('hoveredup1');
+    },
+    function () {
+    $("#cardhalfbg1").removeClass('hoveredup1');
+    }
+    );
+
+    $("#card1").hover(
+    function () {
+    $("#cardhalfbgreverse1").addClass('hovereddown1');
+    },
+    function () {
+    $("#cardhalfbgreverse1").removeClass('hovereddown1');
+    }
+    );
+
+
+    $("#card1").hover(
+    function () {
+    $("#title1").addClass('hovtitle1');
+    },
+    function () {
+    $("#title1").removeClass('hovtitle1');
+    }
+    );
+
+
+
+    /*Card 2*/
+
+    $("#card2").hover(
+    function () {
+    $("#cardhalfbg2").addClass('hoveredup2');
+    },
+    function () {
+    $("#cardhalfbg2").removeClass('hoveredup2');
+    }
+    );
+
+    $("#card2").hover(
+    function () {
+    $("#cardhalfbgreverse2").addClass('hovereddown2');
+    },
+    function () {
+    $("#cardhalfbgreverse2").removeClass('hovereddown2');
+    }
+    );
+
+    $("#card2").hover(
+    function () {
+    $("#title2").addClass('hovtitle2');
+    },
+    function () {
+    $("#title2").removeClass('hovtitle2');
+    }
+    );
+
+
+    /*Card 3*/
+
+    $("#card3").hover(
+    function () {
+    $("#cardhalfbg3").addClass('hoveredup3');
+    },
+    function () {
+    $("#cardhalfbg3").removeClass('hoveredup3');
+    }
+    );
+
+    $("#card3").hover(
+    function () {
+    $("#cardhalfbgreverse3").addClass('hovereddown3');
+    },
+    function () {
+    $("#cardhalfbgreverse3").removeClass('hovereddown3');
+    }
+    );
+
+    $("#card3").hover(
+    function () {
+    $("#title3").addClass('hovtitle3');
+    },
+    function () {
+    $("#title3").removeClass('hovtitle3');
+    }
+    );
+
+    
